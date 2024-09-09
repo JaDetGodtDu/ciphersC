8edd7a5aec451ba0dd24882f46435796d1842290
JaDetGodtDu <123992392+JaDetGodtDu@users.noreply.github.com>
2024-09-09 11:29:41 +0200

    not working

---
+++ caesar.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "cipher.h"
+#include "caesar.h"
+
+void menu() {
+    char choice;
+    char text[255];
+    int shiftvalue;
+
+    printf("Choose (e)ncrypt or (d)ecrypt: ");
+    scanf(" %c", &choice);
+    getchar(); // Consume newline character
+
+    printf("Enter text: ");
+    fgets(text, 255, stdin);
+    // Remove newline character manually
+    for (int i = 0; i < 255; i++) {
+        if (text[i] == '\n') {
+            text[i] = '\0';
+            break;
+        }
+    }
+
+    printf("Enter shift value: ");
+    scanf("%d", &shiftvalue);
+
+    init();
+
+    if (choice == 'e') {
+        encrypt(text, shiftvalue);
+        printf("Encrypted text: %s\n", text);
+    } else if (choice == 'd') {
+        decrypt(text, shiftvalue);
+        printf("Decrypted text: %s\n", text);
+    } else {
+        printf("Invalid choice.\n");
+    }
+}
+
+int main() {
+    menu();
+    return 0;
+}
---
+++ cipher.c
@@ -0,0 +1,54 @@
+#include "cipher.h"
+#include <ctype.h>
+#include <stdio.h>
+
+char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
+int alphabet_length;
+
+void init(){
+    printf("Initializing cipher\n");
+    alphabet_length = sizeof(alphabet) / sizeof(alphabet[0]);
+}
+
+int letterToNumber(char letter){
+    letter = toupper(letter);
+    for (int i = 0; i < alphabet_length; i++) {
+        if (alphabet[i] == letter) {
+            return i + 1;
+        }
+    }
+    return 0;
+}
+
+char numberToLetter(int number){
+    if (number < 1 || number > alphabet_length) {
+        return ' '; // Return space for out-of-bound numbers
+    }
+    return alphabet[number - 1];
+}
+
+int shift(int number, int shiftvalue){
+    int shifted = (number + shiftvalue) % alphabet_length;
+    if (shifted <= 0) {
+        shifted += alphabet_length;
+    }
+    return shifted;
+}
+
+void encrypt(char* text, int shiftvalue){
+    for (int i = 0; text[i] != '\0'; i++) {
+        int number = letterToNumber(text[i]);
+        if (number != 0) {
+            text[i] = numberToLetter(shift(number, shiftvalue));
+        }
+    }
+}
+
+void decrypt(char *text, int shiftvalue){
+    for (int i = 0; text[i] != '\0'; i++) {
+        int number = letterToNumber(text[i]);
+        if (number != 0) {
+            text[i] = numberToLetter(shift(number, -shiftvalue));
+        }
+    }
+}
